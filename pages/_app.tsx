e907a2a91278a1b59b8433eb38fb5674c8a9f5a8
fej0se <80367187+fej0se@users.noreply.github.com>
2021-04-20 18:45:33 -0300

    player component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,20 @@
-import '../styles/global.scss'
+import '../styles/global.scss';
+
+import { Header } from '../src/components/Header';
+import { Player } from '../src/components/Player';
+
+import styles from '../styles/app.module.scss';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className={styles.wrapper}>
+      <main>
+        <Header />
+        <Component {...pageProps} />
+      </main>
+      <Player />
+    </div>
+  )
 }
 
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { Header } from "../src/components/Header";
 
 export default function Home() {
   return (
-    <Header />
+    <h1>Podcastr</h1>
+
 
   )
 }
