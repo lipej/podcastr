4f7ecd525767a3218faf55d26dd7a672ec57dc2a
fej0se <80367187+fej0se@users.noreply.github.com>
2021-04-20 19:10:23 -0300

    consuming a fake api & incremental SSG

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-export default function Home() {
+export default function Home(props) {
   return (
-    <h1>Podcastr</h1>
+    <div>
 
+    <h1>Podcastr</h1>
+    <p>{JSON.stringify(props.episodes)}</p>
+
+    </div>
 
   )
+}
+
+export async function getStaticProps(){
+  const url = 'http://localhost:3333/episodes'
+  
+  const response = await fetch(url)
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8
+  }
 }
